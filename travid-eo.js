e9d02e4862842263b900fc1834b3c24581164cb3
fbiville <florent.biville@vidal.fr>
2012-06-20 13:27:38 +0200

    First version full of awesomeness

---
+++ travid-eo.js
@@ -0,0 +1,7 @@
+var body = document.querySelector('body');
+
+var video = document.createElement("iframe");
+video.src = "http://www.youtube.com/embed/G1f2M5G9KG8?autoplay=1";
+video.id = "travideo";
+
+body.appendChild(video);
